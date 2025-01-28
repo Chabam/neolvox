dbb7c15f8ca4c9eb94dcb3b294c4e37c3ffd9861
Chabam <fchabot1337@gmail.com>
2025-01-28 14:33:18 -0500

    Compiled ctlibmodels

--- external/dependencies.cmake
+++ external/dependencies.cmake
@@ -1,10 +1,13 @@
 # NOTE: using find_package for the moment, may be fallback to
 # fetchContent in the future?
 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules")
+list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/external/cmake/modules")
 
-# qt5-qtbase-devel
-find_package(Qt5 COMPONENTS Widgets REQUIRED)
+# Qt settings
+set(CMAKE_AUTOMOC ON)
+set(CMAKE_AUTORCC ON)
+set(CMAKE_AUTOUIC ON)
+find_package(Qt5 COMPONENTS REQUIRED Widgets Core Gui)
 
 find_package(Eigen3 REQUIRED)
 find_package(GDAL REQUIRED)
