722b281115100d40d30d433be4fa9d60662ff66d
Chabam <fchabot1337@gmail.com>
2025-02-17 16:03:49 +0100

    Made the build work from a top project

--- external/dependencies.cmake
+++ external/dependencies.cmake
@@ -1,8 +1,6 @@
 # NOTE: using find_package for the moment, may be fallback to
 # fetchContent in the future?
 
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/cmake/modules")
-
 # Qt settings
 set(CMAKE_AUTOMOC ON)
 set(CMAKE_AUTORCC ON)
@@ -11,10 +9,12 @@ find_package(Qt5 COMPONENTS REQUIRED Widgets Core Gui Xml)
 
 find_package(muparser REQUIRED)
 
-include(FetchContent)
+if (PROJECT_IS_TOP_LEVEL)
+    include(FetchContent)
 
-FetchContent_Declare(ctlibs
-    GIT_REPOSITORY https://github.com/Chabam/computreelibs
-    GIT_TAG        main
-)
-FetchContent_MakeAvailable(ctlibs)
+    FetchContent_Declare(ctlibs
+        GIT_REPOSITORY https://github.com/Chabam/computreelibs
+        GIT_TAG        main
+    )
+    FetchContent_MakeAvailable(ctlibs)
+endif()
