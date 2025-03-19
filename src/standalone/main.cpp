16462b937b5149b51f3b231f1873f679a49abf16
Chabam <fchabot1337@gmail.com>
2025-03-19 12:07:34 -0400

    Properly compiling and launching with PDAL

--- src/standalone/main.cpp
+++ src/standalone/main.cpp
@@ -1,15 +1,73 @@
-#include <pdal/PointView.hpp>
 #include <iostream>
+#include <format>
+#include <filesystem>
 
-constexpr auto s_invalid_args_msg = "Error invalid amount of arguments supplied";
-constexpr auto s_usage_msg = "Usage: lvox <LASFILE>";
+#include <memory>
+#include <pdal/PointView.hpp>
+#include <pdal/PointTable.hpp>
+#include <pdal/Dimension.hpp>
+#include <pdal/Options.hpp>
+#include <pdal/QuickInfo.hpp>
+#include <pdal/io/BufferReader.hpp>
+#include <pdal/StageFactory.hpp>
+#include <pdal/io/LasReader.hpp>
+#include <stdexcept>
 
-int main(int argc, char* argv[])
+auto print_usage() -> void
 {
-    if (argc == 2)
+
+    std::cout << "Usage: lvox POINT_CLOUD_FILE" << std::endl;
+}
+
+auto print_pts_cloud_metadata(const std::filesystem::path& filename, const pdal::QuickInfo& info) -> void
+{
+    std::cout << std::format(
+            "Loading:\t\t{}\nAmount of points:\t{}",
+            filename.filename().string(),
+            info.m_pointCount)
+        << std::endl;
+}
+
+auto main(int argc, char* argv[]) -> int
+{
+    if (argc < 2)
     {
-        std::cout << s_invalid_args_msg << "\n" << s_usage_msg << std::endl;
+        std::cout << "Invalid amount of arguments supplied" << std::endl;
+        print_usage();
+        return 1;
     }
 
-    std::string_view las_filepath{argv[1]};
+
+    namespace fs = std::filesystem;
+    fs::path filename{argv[1]};
+
+    pdal::Options options;
+    options.add("filename", filename.c_str());
+
+    pdal::PointTable pts_table;
+    pts_table.layout()->registerDim(pdal::Dimension::Id::X);
+    pts_table.layout()->registerDim(pdal::Dimension::Id::Y);
+    pts_table.layout()->registerDim(pdal::Dimension::Id::Z);
+
+
+    pdal::PointViewPtr view{std::make_shared<pdal::PointView>(pts_table)};
+
+    std::unique_ptr<pdal::Reader> file_reader;
+    const std::string extension = filename.extension().string().substr(1);
+    if (extension == "las" or extension == "laz")
+    {
+        file_reader = std::make_unique<pdal::LasReader>();
+    }
+    else
+    {
+        std::cout << std::format("Unsupported file extension: '{}'", extension) << std::endl;
+        return 1;
+    }
+
+    file_reader->setOptions(options);
+    file_reader->prepare(pts_table);
+
+    print_pts_cloud_metadata(filename, file_reader->preview());
+
+    file_reader->execute(pts_table);
 }
