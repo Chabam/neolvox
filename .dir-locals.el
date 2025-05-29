d4d5bdad8a31e420a22bba9fc3a3b4525dc59c3e
Chabam <fchabot1337@gmail.com>
2025-05-29 11:56:57 -0400

    Adding .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+((c++-ts-mode
+  (eval . (setq ff-search-directories
+                (mapcar (lambda (location)
+                          (expand-file-name location (project-root (project-current))))
+                        '("." "include" "include/lvox" "src/standalone/src"))))))
