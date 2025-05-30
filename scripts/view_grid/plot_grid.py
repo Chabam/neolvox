be8026599afe8fc59ecc6a32af95d0a17530725e
Chabam <fchabot1337@gmail.com>
2025-05-30 17:28:19 -0400

    Adding an actual viewer that seems promising

--- scripts/view_grid/plot_grid.py
+++ scripts/view_grid/plot_grid.py
@@ -1,7 +1,9 @@
 import h5py
-import matplotlib.pyplot as plt
-import numpy
-import argparse
+import napari
 
 
-file = h5py.File("tiny_one_scan_lvox_out.h5", "r")
+file = h5py.File("cubes_lvox_out.h5", "r")
+hits_dset = file["lengths"]
+viewer = napari.Viewer(ndisplay=3)
+viewer.add_image(hits_dset[:])
+napari.run()
