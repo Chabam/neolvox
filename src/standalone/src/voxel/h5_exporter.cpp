cfc2c1f4f96291caf6134498c3c96a2ac718a0d6
Chabam <fchabot1337@gmail.com>
2025-05-31 16:33:19 -0400

    Now exporting data as column-major grids

--- src/standalone/src/voxel/h5_exporter.cpp
+++ src/standalone/src/voxel/h5_exporter.cpp
@@ -1,40 +1,59 @@
 #include <H5Fpublic.h>
+#include <cstdint>
+#include <filesystem>
+
 #include <lvox/voxel/h5_exporter.hpp>
+
+#include "lvox/voxel/concrete_grid.hpp"
 
 namespace lvox::h5_exporter
 {
 
-struct H5GridCreationOptions
-{
-    std::string  m_filename;
-    std::string  m_dataset;
-    H5::PredType m_type;
-};
-
 template <typename GridType>
-auto write_grid_to_h5(const H5GridCreationOptions& grid_creation_options, const GridType& grid)
-    -> void
+auto write_grid_to_h5(
+    const std::string& filename, const std::string& dataset_name, const GridType& grid
+) -> void
 {
-    H5::H5File output{grid_creation_options.m_filename, H5F_ACC_CREAT | H5F_ACC_RDWR};
+    H5::H5File output;
+    if (std::filesystem::exists(filename))
+    {
+        output = H5::H5File{filename, H5F_ACC_RDWR};
+    }
+    else
+    {
+        output = H5::H5File{filename, H5F_ACC_TRUNC};
+    }
 
     const auto voxel_size = grid.cell_size();
 
     using h5_dimension_t = std::array<hsize_t, 3>;
-    h5_dimension_t dimensions{grid.dim_x(), grid.dim_y(), grid.dim_z()};
+    h5_dimension_t dimensions{grid.dim_z(), grid.dim_y(), grid.dim_x()};
 
     const H5::DataSpace data_space{std::tuple_size_v<h5_dimension_t>, dimensions.data()};
 
-    const auto h5_data_t = grid_creation_options.m_type;
+    using cell_t = contained_type<typename GridType::cell_t>::type;
+    static_assert(
+        std::is_same_v<cell_t, double> || std::is_same_v<cell_t, uint32_t>,
+        "Unsupported grid value type"
+    );
+    H5::PredType h5_data_t = H5::PredType::PREDTYPE_CONST;
+    if constexpr (std::is_same_v<cell_t, double>)
+    {
+        h5_data_t = H5::PredType::NATIVE_DOUBLE;
+    }
+    else if constexpr (std::is_same_v<cell_t, uint32_t>)
+    {
+        h5_data_t = H5::PredType::NATIVE_INT32;
+    }
 
     H5::DSetCreatPropList create_prop_list{};
-    h5_dimension_t        chunk_dims{20, 20, 20};
+    h5_dimension_t        chunk_dims{5, 5, 5};
     create_prop_list.setChunk(3, chunk_dims.data());
     create_prop_list.setDeflate(6);
     create_prop_list.setFillValue(h5_data_t, 0);
 
-    H5::DataSet dataset = output.createDataSet(
-        grid_creation_options.m_dataset, h5_data_t, data_space, create_prop_list
-    );
+    H5::DataSet dataset =
+        output.createDataSet(dataset_name, h5_data_t, data_space, create_prop_list);
 
     const hsize_t        singular_coord_dim[] = {1};
     H5::DataSpace        singular_coord_data_space{1, singular_coord_dim};
@@ -42,7 +61,10 @@ auto write_grid_to_h5(const H5GridCreationOptions& grid_creation_options, const 
 
     for (const auto& [idx, pad_value] : grid)
     {
-        h5_dimension_t offset = grid.index_to_index3d(idx);
+        const auto& [x, y, z] = grid.index_to_index3d(idx);
+
+        // NOTE: Conversion from row-major to column-major.
+        h5_dimension_t offset = {z, y, x};
         data_space.selectHyperslab(H5S_SELECT_SET, singular_3d_coord_count.data(), offset.data());
 
         dataset.write(&pad_value, h5_data_t, singular_coord_data_space, data_space);
@@ -71,29 +93,24 @@ auto export_grid(
 ) -> void
 {
 
-    const std::string     filename = in_file.filename().string();
-    H5GridCreationOptions grid_creation_options{
-        .m_filename = std::format("{}_lvox_out.h5", filename.substr(0, filename.find_last_of("."))),
-        .m_dataset  = dataset,
-        .m_type     = H5::PredType::NATIVE_DOUBLE
-    };
-
-    write_grid_to_h5(grid_creation_options, result);
+    const std::string filename = in_file.filename().string();
+    write_grid_to_h5(
+        std::format("{}_lvox_out.h5", filename.substr(0, filename.find_last_of("."))),
+        dataset,
+        result
+    );
 }
 
 auto export_grid(
     const GridU32& result, const std::string& dataset, const std::filesystem::path& in_file
 ) -> void
 {
-
-    const std::string     filename = in_file.filename().string();
-    H5GridCreationOptions grid_creation_options{
-        .m_filename = std::format("{}_lvox_out.h5", filename.substr(0, filename.find_last_of("."))),
-        .m_dataset  = dataset,
-        .m_type     = H5::PredType::NATIVE_INT32
-    };
-
-    write_grid_to_h5(grid_creation_options, result);
+    const std::string filename = in_file.filename().string();
+    write_grid_to_h5(
+        std::format("{}_lvox_out.h5", filename.substr(0, filename.find_last_of("."))),
+        dataset,
+        result
+    );
 }
 
 } // namespace lvox::h5_exporter
