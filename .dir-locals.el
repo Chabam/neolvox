f2413d3eaf02c91022d1826118ca778e5753d0d6
Chabam <fchabot1337@gmail.com>
2025-08-06 11:21:11 -0400

    Adding default compile-command

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
   (eval . (setq ff-search-directories
                 (mapcar (lambda (location)
                           (expand-file-name location (project-root (project-current))))
-                        '("." "include" "include/lvox/*" "src/standalone/src/*"))))))
+                        '("." "include" "include/lvox/*" "src/standalone/src/*")))))
+ (nil . ((compile-command . "make -k -C build/ -j$(nproc)"))))
