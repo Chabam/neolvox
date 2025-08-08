ef489c12daa693efe0662ee1fb57cdcb748b4764
Chabam <fchabot1337@gmail.com>
2025-08-08 18:14:39 -0400

    Adding a basic benchmark that checks the time taken vs. point cloud size

--- core/tests/utils/utils.cpp
+++ core/tests/utils/utils.cpp
@@ -1,3 +1,6 @@
+#include <chrono>
+#include <random>
+#include <ranges>
 #include <utils/utils.hpp>
 
 #include <pdal/PointTable.hpp>
@@ -5,6 +8,22 @@
 
 #include <lvox/types.hpp>
 #include "lvox/voxel/bounds.hpp"
+
+auto get_current_time_as_gps_time() -> double
+{
+    namespace c = std::chrono;
+    const auto current_time = c::system_clock::now().time_since_epoch();
+    return static_cast<double>(c::duration_cast<c::microseconds>(current_time).count());
+}
+
+Point::Point(double x, double y, double z, double gps_time)
+    : x{x}
+    , y{y}
+    , z{z}
+    , gps_time{gps_time}
+{
+}
+
 
 auto create_bounds(double dim_x, double dim_y, double dim_z) -> lvox::Bounds
 {
@@ -18,16 +37,10 @@ auto create_bounds(double dim_x, double dim_y, double dim_z) -> lvox::Bounds
 auto generate_cubic_point_cloud(pdal::PointTableRef table, double dim_x, double dim_y, double dim_z)
     -> pdal::PointViewPtr
 {
-    struct Point
-    {
-        double x;
-        double y;
-        double z;
-    };
-
     table.layout()->registerDim(pdal::Dimension::Id::X);
     table.layout()->registerDim(pdal::Dimension::Id::Y);
     table.layout()->registerDim(pdal::Dimension::Id::Z);
+    table.layout()->registerDim(pdal::Dimension::Id::GpsTime);
 
     pdal::PointViewPtr view{std::make_unique<pdal::PointView>(table)};
     const double       half_dim_x = dim_x / 2.0;
@@ -52,6 +65,35 @@ auto generate_cubic_point_cloud(pdal::PointTableRef table, double dim_x, double 
         view->setField(pdal::Dimension::Id::X, i, cube[i].x);
         view->setField(pdal::Dimension::Id::Y, i, cube[i].y);
         view->setField(pdal::Dimension::Id::Z, i, cube[i].z);
+    }
+
+    return view;
+}
+
+auto generate_cubic_point_cloud_with_random_points(pdal::PointTableRef table, size_t point_count, double dim_x, double dim_y, double dim_z)
+    -> pdal::PointViewPtr
+{
+    table.layout()->registerDim(pdal::Dimension::Id::X);
+    table.layout()->registerDim(pdal::Dimension::Id::Y);
+    table.layout()->registerDim(pdal::Dimension::Id::Z);
+    table.layout()->registerDim(pdal::Dimension::Id::GpsTime);
+
+    pdal::PointViewPtr view{std::make_unique<pdal::PointView>(table)};
+    const double       half_dim_x = dim_x / 2.0;
+    const double       half_dim_y = dim_y / 2.0;
+    const double       half_dim_z = dim_z / 2.0;
+
+    // Fixing the seed for reproducibility. Maybe not that useful?
+    std::mt19937 gen{1337};
+    std::uniform_real_distribution<double> dis_x(-half_dim_x, half_dim_x);
+    std::uniform_real_distribution<double> dis_y(-half_dim_y, half_dim_y);
+    std::uniform_real_distribution<double> dis_z(-half_dim_z, half_dim_z);
+
+    for (lvox::Index i = 0; i < point_count; ++i)
+    {
+        view->setField(pdal::Dimension::Id::X, i, dis_x(gen));
+        view->setField(pdal::Dimension::Id::Y, i, dis_y(gen));
+        view->setField(pdal::Dimension::Id::Z, i, dis_z(gen));
     }
 
     return view;
--- core/tests/utils/utils.hpp
+++ core/tests/utils/utils.hpp
@@ -5,7 +5,17 @@
 #include <pdal/PointTable.hpp>
 #include <pdal/util/Bounds.hpp>
 
-#include <lvox/voxel/bounds.hpp>
+auto get_current_time_as_gps_time() -> double;
+
+struct Point
+{
+    Point(double x, double y, double z, double gps_time = get_current_time_as_gps_time());
+
+    double x;
+    double y;
+    double z;
+    double gps_time;
+};
 
 // Creates a bound from -(dim/2) to (dim/2)
 auto create_bounds(double dim_x, double dim_y, double dim_z) -> lvox::Bounds;
@@ -14,5 +24,11 @@ auto create_bounds(double dim_x, double dim_y, double dim_z) -> lvox::Bounds;
 auto generate_cubic_point_cloud(
     pdal::PointTableRef table, double dim_x = 2., double dim_y = 2., double dim_z = 2.
 ) -> pdal::PointViewPtr;
+
+// Generates a point cloud of N points all located within the given cubic bounds.
+auto generate_cubic_point_cloud_with_random_points(
+    pdal::PointTableRef table, size_t point_count, double dim_x = 2., double dim_y = 2., double dim_z = 2.
+) -> pdal::PointViewPtr;
+
 
 #endif // !LVOX_TEST_GRID_HELPERS
