42d02edfd6792c63415c1ab5b6c793d63f6b84c9
Chabam <fchabot1337@gmail.com>
2025-12-15 14:19:40 -0500

    Fixed a variable to not be the same as an R base function

--- R/R/LvoxGrid.R
+++ R/R/LvoxGrid.R
@@ -7,19 +7,19 @@ print.LVoxGrid <- function(x, ...) {
 #' @description Computes the mean PAD value for each vertical layer of the grid
 #' @param x A LVoxGrid object
 #' @return A LvoxHeightProfile object
-computeHeightProfile <- function(grid) {
-  if (class(grid) != "LVoxGrid")
+computeHeightProfile <- function(lvox_grid) {
+  if (!inherits(lvox_grid, "LVoxGrid"))
     error("Input must be an LVoxGrid object")
-  pad <- grid$data$PAD
+  pad <- lvox_grid$data$PAD
   pad[is.nan(pad)] <- 0
   pad[is.infinite(pad)] <- 0
 
-  z_count <- grid$Dimensions[3] - 1
+  z_count <- lvox_grid$Dimensions[3] - 1
 
   profile <- sapply(seq(0, z_count), function(z) {
-    mean(grid$data$PAD[grid$data$Z == z])
+    mean(lvox_grid$data$PAD[lvox_grid$data$Z == z])
   })
-  profile_real_height <- seq(0, z_count) * grid$VoxelSize + grid$MinimalCoords[3]
+  profile_real_height <- seq(0, z_count) * lvox_grid$VoxelSize + lvox_grid$MinimalCoords[3]
 
   structure(list(Heights = profile_real_height, PAD = profile), class = "LVoxHeightProfile")
 }
