7ef4982d3b172121c989fd359ffd1488158f9820
Chabam <fchabot1337@gmail.com>
2025-12-19 11:14:26 -0500

    loading_tls with real data

--- scripts/R/loading_tls.R
+++ scripts/R/loading_tls.R
@@ -1,9 +1,26 @@
 library(lidR)
 library(lvoxR)
 
-tls <- list(readLAS("~/Documents/Données lidar/ALS/SG_IR057.las"))
-origins <- list(c(0,0,0))
+tls <- list(
+  readLAS("~/Documents/Données lidar/PRF133/TLS/Petawawa_133_Scan_000.laz"),
+  readLAS("~/Documents/Données lidar/PRF133/TLS/Petawawa_133_Scan_001.laz"),
+  readLAS("~/Documents/Données lidar/PRF133/TLS/Petawawa_133_Scan_002.laz"),
+  readLAS("~/Documents/Données lidar/PRF133/TLS/Petawawa_133_Scan_003.laz"),
+  readLAS("~/Documents/Données lidar/PRF133/TLS/Petawawa_133_Scan_004.laz"))
 
+origins <- list(
+  c(0, 0, 94.131491),
+  c(0.506694, 12.121184, 93.97213),
+  c(11.452984, -1.91865, 93.727236),
+  c(-0.821686, -10.803126, 94.414889),
+  c(11.340925, 2.670768, 94.75565)
+)
+dense_grid <- as.matrix(grid)
+dense_grid[is.nan(dense_grid)] <- 0
+dense_grid[is.infinite(dense_grid)] <- 0
+profile_real_height <- seq(0, grid$Dimensions[3] - 1) * grid$VoxelSize + grid$MinimalCoords[3]
+
+  profile <- apply(dense_grid, MARGIN = c(1,2), FUN = mean)
 grid <- lvoxComputeTLS(tls, origins, threadCount = 12)
 hp <- computeHeightProfile(grid)
 hp
