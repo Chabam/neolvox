5b44134b6f55ce99a7669902ca3d03e362d6711d
Chabam <fchabot1337@gmail.com>
2026-02-03 16:03:42 -0500

    Fixed again the dir-locals.el for real this time

--- paper/.dir-locals.el
+++ paper/.dir-locals.el
@@ -1,6 +1,7 @@
 ((markdown-mode
-  . (eglot-workspace-configuration
-     . ((:ltex (:language "en"
-                :ignoreOptionalArguments t
-                :completionEnabled nil
-                :diagnosticSeverity "information"))))))
+  . ((eglot-workspace-configuration
+      . ((:ltex
+          . (:language "en"
+                       :ignoreOptionalArguments t
+                       :completionEnabled nil
+                       :diagnosticSeverity "information")))))))
