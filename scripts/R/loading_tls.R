d29df673e5e4b91d920696336fa9b5fe6f825798
Chabam <fchabot1337@gmail.com>
2026-02-20 12:38:52 -0500

    Updating script to take the new arguments

--- scripts/R/loading_tls.R
+++ scripts/R/loading_tls.R
@@ -2,26 +2,30 @@ library(lidR)
 library(lvoxR)
 
 tls <- list(
-  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_000.laz")## ,
-  ## readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_001.laz"),
-  ## readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_002.laz"),
-  ## readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_003.laz"),
-  ## readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_004.laz")
+  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_000.laz"),
+  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_001.laz"),
+  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_002.laz"),
+  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_003.laz"),
+  readLAS("~/Documents/Lidar/PRF133/TLS/Petawawa_133_Scan_004.laz")
 )
 
 origins <- list(
-  c(0, 0, 94.131491)## ,
-  ## c(0.506694, 12.121184, 93.97213),
-  ## c(11.452984, -1.91865, 93.727236),
-  ## c(-0.821686, -10.803126, 94.414889),
-  ## c(11.340925, 2.670768, 94.75565)
+  c(0, 0, 94.131491),
+  c(0.506694, 12.121184, 93.97213),
+  c(11.452984, -1.91865, 93.727236),
+  c(-0.821686, -10.803126, 94.414889),
+  c(11.340925, 2.670768, 94.75565)
 )
+lowestPoint <- c(-25, -25, 90)
+highestPoint <- c(25, 25, 130)
 grid <- estimatePADForTLS(
   tls,
   origins,
   padEstimator = "BCMLE",
-  smallestElementArea = needleProjectedAreaFromDimensions(0.06, 0.01)
+  smallestElementArea = needleProjectedAreaFromDimensions(0.06, 0.01),
+  bounds = list(lowestPoint, highestPoint)
 )
+
 hp <- computeHeightProfile(grid)
 hp
 plot(hp)
