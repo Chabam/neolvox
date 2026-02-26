1f6be4dbdaa169d5d320d2be2194ef95e411b502
Chabam <fchabot1337@gmail.com>
2026-02-25 21:43:39 -0500

    Fixed the script to fit the new grid format

--- scripts/view_grid/view_grid.py
+++ scripts/view_grid/view_grid.py
@@ -1,16 +1,29 @@
 import h5py
 import napari
 import sparse
+import numpy
 
 
 file = h5py.File("out.h5")
 group: h5py.Group = file["lvox"]
 
 dims = group.attrs["Dimensions"]
-dset_hits = sparse.COO((group["hits"], (group["x"], group["y"], group["z"])), shape=dims).todense()
-dset_counts = sparse.COO((group["counts"], (group["x"], group["y"], group["z"])), shape=dims).todense()
-dset_lengths = sparse.COO((group["lengths"], (group["x"], group["y"], group["z"])), shape=dims).todense()
-dset_pad = sparse.COO((group["pad"], (group["x"], group["y"], group["z"])), shape=dims).todense()
+minIdx = group.attrs["Minimal index values"]
+
+x = group["x"][()]
+y = group["y"][()]
+z = group["z"][()]
+
+xs = x - minIdx[0]
+ys = y - minIdx[1]
+zs = z - minIdx[2]
+
+coords = numpy.vstack([xs, ys, zs])
+
+dset_hits = sparse.COO(coords, group["hits"], shape=dims).todense()
+dset_counts = sparse.COO(coords, group["counts"], shape=dims).todense()
+dset_lengths = sparse.COO(coords, group["lengths"], shape=dims).todense()
+dset_pad = sparse.COO(coords, group["pad"], shape=dims).todense()
 viewer = napari.Viewer(ndisplay=3)
 viewer.add_image(dset_pad[:], name="pad")
 viewer.add_image(dset_hits[:], name="hits")
