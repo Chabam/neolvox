17d16253bfd7de8f922546b9592ada852ffd76c9
Chabam <fchabot1337@gmail.com>
2026-08-28 13:03:48 -0400

    Trying out conan for dependency management

---
+++ conanfile.py
@@ -0,0 +1,18 @@
+from conan import ConanFile
+
+class NeolvoxRecipe(ConanFile):
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    default_options = {
+        "boost/*:without_cobalt": True,
+        "proj/*:build_executables": False,
+    }
+
+    def requirements(self):
+        self.requires("pdal/2.10.1")
+        self.requires("eigen/5.0.1")
+        self.requires("hdf5/1.14.6")
+        self.requires("gtest/1.18.0")
+        self.requires("benchmark/1.9.5")
