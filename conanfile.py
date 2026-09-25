46bbc87bcf1726970318e42c330ffa451a67b1d4
Chabam <fchabot1337@gmail.com>
2026-09-25 15:32:58 -0400

    Trying CI/CD once again

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,13 @@ from conan import ConanFile
 class NeolvoxRecipe(ConanFile):
 
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeToolchain", "CMakeConfigDeps"
 
     default_options = {
         "boost/*:without_cobalt": True,
+        "boost/*:without_stacktrace": True,
         "proj/*:build_executables": False,
+        "arrow/*:parquet": False,
     }
 
     def requirements(self):
@@ -16,3 +18,7 @@ class NeolvoxRecipe(ConanFile):
         self.requires("hdf5/1.14.6")
         self.requires("gtest/1.18.0")
         self.requires("benchmark/1.9.5")
+        if self.settings.os == "Windows" and self.settings.compiler == "gcc":
+            # Outdated dependencies that causes build errors
+            self.requires("thrift/0.24.0", override=True)
+            self.requires("proj/9.7.0", override=True)
