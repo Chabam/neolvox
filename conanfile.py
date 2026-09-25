91babe6995a0e9f820f3f371d9b1946bbd7c5b86
Chabam <fchabot1337@gmail.com>
2026-09-25 15:47:43 -0400

    Cleanup forgotten options when I was trying to cross-compile

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,6 @@ class NeolvoxRecipe(ConanFile):
         "boost/*:without_cobalt": True,
         "boost/*:without_stacktrace": True,
         "proj/*:build_executables": False,
-        "arrow/*:parquet": False,
     }
 
     def requirements(self):
@@ -18,7 +17,3 @@ class NeolvoxRecipe(ConanFile):
         self.requires("hdf5/1.14.6")
         self.requires("gtest/1.18.0")
         self.requires("benchmark/1.9.5")
-        if self.settings.os == "Windows" and self.settings.compiler == "gcc":
-            # Outdated dependencies that causes build errors
-            self.requires("thrift/0.24.0", override=True)
-            self.requires("proj/9.7.0", override=True)
