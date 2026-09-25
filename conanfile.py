347d0cda9c619f3f5f18744d4381b01e7e8323d1
Chabam <fchabot1337@gmail.com>
2026-09-25 15:49:00 -0400

    Adding requirements for a cmake version lower for geos

--- conanfile.py
+++ conanfile.py
@@ -17,3 +17,6 @@ class NeolvoxRecipe(ConanFile):
         self.requires("hdf5/1.14.6")
         self.requires("gtest/1.18.0")
         self.requires("benchmark/1.9.5")
+
+    def tool_requires(self):
+        self.requires("!cmake/*: cmake/[>=3 <4]")
